c0b7bb02a975b4a6daa7a4c3a53ad4c062d66e12
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-26 22:56:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,14 +7,14 @@ end
 WIN_COMBINATIONS = [
 
   [0,1,2], # Top row
-  [3,4,5]  # Middle row
-  [6,7,8]  # Bottom row
+  [3,4,5],  # Middle row
+  [6,7,8], # Bottom row
 
-  [0,3,6]  # First column
-  [1,4,7]  # Middle column
-  [2,5,8]  # Last column
+  [0,3,6],  # First column
+  [1,4,7],  # Middle column
+  [2,5,8],  # Last column
 
-  [0,4,8]  # TopLeft to BottomRight
+  [0,4,8],  # TopLeft to BottomRight
   [2,4,6]  # TopRight to BottomLeft
 
   # ETC, an array for each win combination
